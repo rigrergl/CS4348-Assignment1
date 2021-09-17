e53ed1ec87f7baf8acb34de92e16f1fb334b8b22
rigrergl <rigrergl@gmail.com>
2021-09-17 01:45:11 -0500

    functionality complete

--- program.cpp
+++ program.cpp
@@ -32,8 +32,59 @@ void printMachineInfo()
     std::cout << "--------------------------------------------------------------------" << std::endl;
 }
 
-bool isMagicSquare(char *str){
-    return true; //TODO
+void printSquare(int nums[][3])
+{
+    for (int r = 0; r < 3; r++) 
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            std::cout << nums[r][c] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool isMagicSquare(char *str)
+{
+    int nums[3][3];
+    for (int i = 0; i < 9; i++) {
+        nums[i / 3][i % 3] = str[i] - '0'; //get the int value of an ascii character
+    }
+    printSquare(nums);
+
+    /*
+    To verify that it's a magic square, we will take the sum of the first row
+    Then we will make sure the sum of every other row, column, and diagonal is equal to that
+    Returning false if any of those checks fail
+    */
+    int firstRowSum = nums[0][0] + nums[0][1] + nums[0][2];
+
+    //checking the remaining rows
+    for (int r = 1; r < 3; r++) 
+    {
+        int rowSum = nums[r][0] + nums[r][1] + nums[r][2];
+        if (rowSum != firstRowSum)
+            return false;
+    }
+    //checking columns
+    for (int c = 0; c < 3; c++)
+    {
+        int colSum = nums[0][c] + nums[1][c] + nums[2][c];
+        if (colSum != firstRowSum)
+            return false;
+    }
+    //checking first diagonal
+    int firstDiagonalSum = nums[0][0] + nums[1][1] + nums[2][2];
+    if (firstDiagonalSum != firstRowSum)
+        return false;
+
+    //checking second diagonal
+    int secondDiagonalSum = nums[0][2] + nums[1][1] + nums[2][0];
+    if (secondDiagonalSum != firstRowSum)
+        return false;
+
+    //at this point, we have checked every row, column, and diagonal, and can confirm that we have a magic square
+    return true;
 }
 
 void clientMain(int readingPipe, int writingPipe)
